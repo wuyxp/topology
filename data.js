de03de26de49a1a9a377c7fc5571fd07be128353
wuyang <956826374@qq.com>
2016-06-15 17:59:27 +0800

    增加05版本

--- data.js
+++ data.js
@@ -6,34 +6,28 @@
 //主机接口设计
 
 var nets = {
-    'base' : {
-        'name'  :   'www',
-        'ip'    :   '192.168.1.1',
-        'DHCP'  :   '启用',
-        'hostN' :   6
-    },
     'router' : [
         {
             'rtr_id'    :   'rtr-001',
-            'rtr_name'  :   'rtr-name11',
+            'rtr_name'  :   '路由rtr-001',
             'rtr_status':   '运行中',
             'rtr_ip'    :   '202.201.1.1',
             'rtr_subnet':   2,
         },{
             'rtr_id'    :   'rtr-002',
-            'rtr_name'  :   'rtr-name22',
+            'rtr_name'  :   '路由rtr-002',
             'rtr_status':   '运行中',
             'rtr_ip'    :   '202.201.1.1',
             'rtr_subnet':   1,
         },{
             'rtr_id'    :   'rtr-003',
-            'rtr_name'  :   'rtr-name33',
+            'rtr_name'  :   '路由rtr-003',
             'rtr_status':   '运行中',
             'rtr_ip'    :   '202.201.1.1',
             'rtr_subnet':   1,
         },{
             'rtr_id'    :   'rtr-004',
-            'rtr_name'  :   'rtr-name44',
+            'rtr_name'  :   '路由rtr-004',
             'rtr_status':   '运行中',
             'rtr_ip'    :   '202.201.1.1',
             'rtr_subnet':   1,
@@ -42,36 +36,57 @@ var nets = {
     'subnet' : [
         {
             'net_id'    :   'base-net',
-            'net_name'  :   'base-name-net',
+            'net_name'  :   '直接连入外网子网01',
             'net_ip'    :   '192.168.2.1',
             'net_hostN' :   32,
             'type'      :   'public',
-            'router_id' :   'base'
+            'router_id' :   null
         },
         {
             'net_id'    :   'net-001',
-            'net_name'  :   'net-name001',
+            'net_name'  :   '连入路由器rtr-001子网01',
             'net_ip'    :   '192.168.2.1',
             'net_hostN' :   32,
             'type'      :   'public',
             'router_id' :   'rtr-001'
         },{
             'net_id'    :   'net-002',
-            'net_name'  :   'net-name002',
+            'net_name'  :   '连入路由器rtr-001子网02',
             'net_ip'    :   '192.168.2.1',
             'net_hostN' :   10,
             'type'      :   'public',
             'router_id' :   'rtr-001'
         },{
+            'net_id'    :   'net-002',
+            'net_name'  :   '直接连入外网子网02',
+            'net_ip'    :   '192.168.2.1',
+            'net_hostN' :   10,
+            'type'      :   'public',
+            'router_id' :   null
+        },{
             'net_id'    :   'net-003',
-            'net_name'  :   'net-name003',
+            'net_name'  :   '连入路由器rtr-002子网01',
+            'net_ip'    :   '192.168.2.1',
+            'net_hostN' :   22,
+            'type'      :   'public',
+            'router_id' :   'rtr-002'
+        },{
+            'net_id'    :   'net-002',
+            'net_name'  :   '直接连入外网子网03',
+            'net_ip'    :   '192.168.2.1',
+            'net_hostN' :   10,
+            'type'      :   'public',
+            'router_id' :   null
+        },{
+            'net_id'    :   'net-003',
+            'net_name'  :   '连入路由器rtr-002子网02',
             'net_ip'    :   '192.168.2.1',
             'net_hostN' :   22,
             'type'      :   'public',
             'router_id' :   'rtr-002'
         },{
             'net_id'    :   'net-004',
-            'net_name'  :   'net-name004',
+            'net_name'  :   '连入路由器rtr-003子网01',
             'net_ip'    :   '192.168.2.1',
             'net_hostN' :   6,
             'type'      :   'public',
