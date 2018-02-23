6aa7bb3b388c958fc828c49b7c7f985c00089e3c
wuyang <wuyang@cloudin.cn>
2018-02-23 15:26:32 +0800

    增加图片及07版本

--- data.js
+++ data.js
@@ -6,232 +6,208 @@
 //主机接口设计
 
 var nets = {
-    'router' : [
+    'routers' : [
         {
-            'rtr_id'    :   'rtr-001',
-            'rtr_name'  :   '路由rtr-001',
-            'rtr_status':   '运行中',
-            'rtr_ip'    :   '202.201.1.1',
-            'rtr_subnet':   2,
+            'router_id'    :   'rtr-001',
+            'router_name'  :   '路由rtr-001',
+            'status':   '运行中',
+            'external_ips'    :   '202.201.1.1',
+            'subnet_count':   2,
         },{
-            'rtr_id'    :   'rtr-002',
-            'rtr_name'  :   '路由rtr-002',
-            'rtr_status':   '运行中',
-            'rtr_ip'    :   '202.201.1.1',
-            'rtr_subnet':   1,
+            'router_id'    :   'rtr-002',
+            'router_name'  :   '路由rtr-002',
+            'status':   '运行中',
+            'external_ips'    :   '202.201.1.1',
+            'subnet_count':   1,
         },{
-            'rtr_id'    :   'rtr-003',
-            'rtr_name'  :   '路由rtr-003',
-            'rtr_status':   '运行中',
-            'rtr_ip'    :   '202.201.1.1',
-            'rtr_subnet':   1,
+            'router_id'    :   'rtr-003',
+            'router_name'  :   '路由rtr-003',
+            'status':   '运行中',
+            'external_ips'    :   '202.201.1.1',
+            'subnet_count':   1,
         },{
-            'rtr_id'    :   'rtr-004',
-            'rtr_name'  :   '路由rtr-004',
-            'rtr_status':   '运行中',
-            'rtr_ip'    :   '202.201.1.1',
-            'rtr_subnet':   1,
+            'router_id'    :   'rtr-004',
+            'router_name'  :   '路由rtr-004',
+            'status':   '运行中',
+            'external_ips'    :   '202.201.1.1',
+            'subnet_count':   1,
         }
     ],
-    'subnet' : [
+    'nets' : [
         {
             'net_id'    :   'base-net',
             'net_name'  :   '直接连入外网子网01',
-            'net_ip'    :   '192.168.2.1',
-            'net_hostN' :   32,
+            'cidr'    :   '192.168.2.1',
+            'instances_count' :   32,
             'type'      :   'public',
-            'router_id' :   null
+            'router_id' :   null,
+            'enable_dhcp' : true,
+            'gateway_ip' : '10.1.1.1'
         },
         {
             'net_id'    :   'net-001',
             'net_name'  :   '连入路由器rtr-001子网01',
-            'net_ip'    :   '192.168.2.1',
-            'net_hostN' :   32,
+            'cidr'    :   '192.168.2.1',
+            'instances_count' :   32,
             'type'      :   'public',
-            'router_id' :   'rtr-001'
+            'router_id' :   'rtr-001',
+            'enable_dhcp' : true,
+            'gateway_ip' : '10.1.1.1'
         },{
             'net_id'    :   'net-002',
             'net_name'  :   '连入路由器rtr-001子网02',
-            'net_ip'    :   '192.168.2.1',
-            'net_hostN' :   10,
+            'cidr'    :   '192.168.2.1',
+            'instances_count' :   10,
             'type'      :   'public',
-            'router_id' :   'rtr-001'
+            'router_id' :   'rtr-001',
+            'enable_dhcp' : true,
+            'gateway_ip' : '10.1.1.1'
         },{
             'net_id'    :   'net-003',
             'net_name'  :   '直接连入外网子网02',
-            'net_ip'    :   '192.168.2.1',
-            'net_hostN' :   10,
+            'cidr'    :   '192.168.2.1',
+            'instances_count' :   10,
             'type'      :   'public',
-            'router_id' :   null
+            'router_id' :   null,
+            'enable_dhcp' : true,
+            'gateway_ip' : '10.1.1.1'
         },{
             'net_id'    :   'net-004',
             'net_name'  :   '连入路由器rtr-002子网01',
-            'net_ip'    :   '192.168.2.1',
-            'net_hostN' :   22,
+            'cidr'    :   '192.168.2.1',
+            'instances_count' :   22,
             'type'      :   'public',
-            'router_id' :   'rtr-002'
+            'router_id' :   'rtr-002',
+            'enable_dhcp' : true,
+            'gateway_ip' : '10.1.1.1'
         },{
             'net_id'    :   'net-005',
             'net_name'  :   '直接连入外网子网03',
-            'net_ip'    :   '192.168.2.1',
-            'net_hostN' :   10,
+            'cidr'    :   '192.168.2.1',
+            'instances_count' :   10,
             'type'      :   'public',
-            'router_id' :   null
+            'router_id' :   null,
+            'enable_dhcp' : true,
+            'gateway_ip' : '10.1.1.1'
         },{
             'net_id'    :   'net-006',
             'net_name'  :   '连入路由器rtr-002子网02',
-            'net_ip'    :   '192.168.2.1',
-            'net_hostN' :   22,
+            'cidr'    :   '192.168.2.1',
+            'instances_count' :   22,
             'type'      :   'public',
-            'router_id' :   'rtr-002'
+            'router_id' :   'rtr-002',
+            'enable_dhcp' : true,
+            'gateway_ip' : '10.1.1.1'
         },{
             'net_id'    :   'net-007',
             'net_name'  :   '连入路由器rtr-003子网01',
-            'net_ip'    :   '192.168.2.1',
-            'net_hostN' :   6,
+            'cidr'    :   '192.168.2.1',
+            'instances_count' :   6,
             'type'      :   'public',
-            'router_id' :   'rtr-003'
+            'router_id' :   'rtr-003',
+            'enable_dhcp' : true,
+            'gateway_ip' : '10.1.1.1'
         }
     ]
 };
 
 var hosts = [
     {
-        'id'            :   'host-zasgt25a',
-        'name'          :   '开发1',
-        'status'        :   '运行中',
+        'instance_id'   :   'host-zasgt25a',
+        'instance_name' :   '开发1',
+        'instance_state':   '运行中',
         'net'           :   'www',
-        'ip'            :   '192.16.1.2',
-        'mirror'        :   'Windows Server 2008 R2 Datacenter 简体中文 未激活',
+        'image'         :   'Windows Server 2008 R2 Datacenter 简体中文 未激活',
         'create_date'   :   1464135759,
-        'net_id'        :   ['base-net','net-002']
+        'nets'          :   [{
+            'id' :'base-net',
+            'ip' : '192.168.0.1'
+        },{
+            'id' : 'net-002',
+            'ip' : '192.168.0.2'
+        }]
     },{
-        'id'            :   'host-zasgt25a',
-        'name'          :   '开发1',
-        'status'        :   '运行中',
+        'instance_id'   :   'host-zadgs01a',
+        'instance_name' :   '开发2',
+        'instance_state':   '运行中',
         'net'           :   'www',
-        'ip'            :   '192.16.1.2',
-        'mirror'        :   'Windows Server 2008 R2 Datacenter 简体中文',
+        'image'         :   'Windows Server 2008 R',
         'create_date'   :   1464135759,
-        'net_id'        :   ['base-net','net-001','net-002']
+        'nets'          :   [{
+            'id' :'base-net',
+            'ip' : '192.168.0.1'
+        },{
+            'id' : 'net-001',
+            'ip' : '192.168.0.2'
+        },{
+            'id' : 'net-002',
+            'ip' : '192.168.0.2'
+        }]
     },{
-        'id'            :   'host-zasgt25a',
-        'name'          :   '开发1',
-        'status'        :   '运行中',
+        'instance_id'   :   'host-zadgs01a',
+        'instance_name' :   '开发2',
+        'instance_state':   '运行中',
         'net'           :   'www',
-        'ip'            :   '192.16.1.2',
-        'mirror'        :   'Windows Server 2008 R2 Datacenter 简体中文 未激活',
+        'image'         :   'Windows Server 2008 R',
         'create_date'   :   1464135759,
-        'net_id'        :   ['base-net','net-001']
-    },
-    {
-        'id'            :   'host-zasgt25a',
-        'name'          :   '开发1',
-        'status'        :   '运行中',
-        'net'           :   'www',
-        'ip'            :   '192.16.1.2',
-        'mirror'        :   'Windows Server 2008 R2 Datacenter 简体中文 未激活',
-        'create_date'   :   1464135759,
-        'net_id'        :   ['net-001']
+        'nets'          :   [{
+            'id' : 'net-003',
+            'ip' : '192.168.0.2'
+        },{
+            'id' : 'net-004',
+            'ip' : '192.168.0.2'
+        }]
     },{
-        'id'            :   'host-zasgt25a',
-        'name'          :   '开发1',
-        'status'        :   '运行中',
+        'instance_id'   :   'host-zadgs01a',
+        'instance_name' :   '开发2',
+        'instance_state':   '运行中',
         'net'           :   'www',
-        'ip'            :   '192.16.1.2',
-        'mirror'        :   'Windows Server 2008 R2 Datacenter 简体中文',
+        'image'         :   'Windows Server 2008 R',
         'create_date'   :   1464135759,
-        'net_id'        :   ['net-001']
+        'nets'          :   [{
+            'id' :'base-net',
+            'ip' : '192.168.0.1'
+        },{
+            'id' : 'net-001',
+            'ip' : '192.168.0.2'
+        },{
+            'id' : 'net-002',
+            'ip' : '192.168.0.2'
+        }]
     },{
-        'id'            :   'host-zasgt25a',
-        'name'          :   '开发1',
-        'status'        :   '运行中',
+        'instance_id'   :   'host-zadgs01a',
+        'instance_name' :   '开发2',
+        'instance_state':   '运行中',
         'net'           :   'www',
-        'ip'            :   '192.16.1.2',
-        'mirror'        :   'Windows Server 2008 R2 Datacenter 简体中文 未激活',
+        'image'         :   'Windows Server 2008 R',
         'create_date'   :   1464135759,
-        'net_id'        :   ['net-002']
+        'nets'          :   [{
+            'id' :'base-net',
+            'ip' : '192.168.0.1'
+        },{
+            'id' : 'net-001',
+            'ip' : '192.168.0.2'
+        },{
+            'id' : 'net-002',
+            'ip' : '192.168.0.2'
+        }]
     },{
-        'id'            :   'host-zasgt25a',
-        'name'          :   '开发1',
-        'status'        :   '运行中',
+        'instance_id'   :   'host-zadgs01a',
+        'instance_name' :   '开发2',
+        'instance_state':   '运行中',
         'net'           :   'www',
-        'ip'            :   '192.16.1.2',
-        'mirror'        :   'Windows',
-        'create_date'   :   146,
-        'net_id'        :   ['net-002']
-    },{
-        'id'            :   'host-zasgt25a',
-        'name'          :   '开发1',
-        'status'        :   '运行中',
-        'net'           :   'www',
-        'ip'            :   '192.16.1.2',
-        'mirror'        :   'Windows Server 2008 R2 Datacenter 简体中文 未激活',
+        'image'         :   'Windows Server 2008 R',
         'create_date'   :   1464135759,
-        'net_id'        :   ['net-002']
-    },{
-        'id'            :   'host-zasgt25a',
-        'name'          :   '开发1',
-        'status'        :   '运行中',
-        'net'           :   'www',
-        'ip'            :   '192.16.1.2',
-        'mirror'        :   'Windows Server 2008 R2 Datacenter 简体中文 未激活',
-        'create_date'   :   1464135759,
-        'net_id'        :   ['net-003']
-    },{
-        'id'            :   'host-zasgt25a',
-        'name'          :   '开发1',
-        'status'        :   '运行中',
-        'net'           :   'www',
-        'ip'            :   '192.16.1.2',
-        'mirror'        :   'Windows Server 2008 R2 Datacenter 简体中文 未激活',
-        'create_date'   :   1464135759,
-        'net_id'        :   ['net-003']
-    },{
-        'id'            :   'host-zasgt25a',
-        'name'          :   '开发1',
-        'status'        :   '运行中',
-        'net'           :   'www',
-        'ip'            :   '192.16.1.2',
-        'mirror'        :   'Windows Server 2008 R2 Datacenter 简体中文 未激活',
-        'create_date'   :   1464135759,
-        'net_id'        :   ['net-003']
-    },{
-        'id'            :   'host-zasgt25a',
-        'name'          :   '开发1',
-        'status'        :   '运行中',
-        'net'           :   'www',
-        'ip'            :   '192.16.1.2',
-        'mirror'        :   'Windows Server 2008 R2 Datacenter 简体中文 未激活',
-        'create_date'   :   1464135759,
-        'net_id'        :   ['net-004']
-    },{
-        'id'            :   'host-zasgt25a',
-        'name'          :   '开发1',
-        'status'        :   '运行中',
-        'net'           :   'www',
-        'ip'            :   '192.16.1.2',
-        'mirror'        :   'Windows Server 2008 R2 Datacenter 简体中文 未激活',
-        'create_date'   :   1464135759,
-        'net_id'        :   ['net-004']
-    },{
-        'id'            :   'host-zasgt25a',
-        'name'          :   '开发1',
-        'status'        :   '运行中',
-        'net'           :   'www',
-        'ip'            :   '192.16.1.2',
-        'mirror'        :   'Windows Server 2008 R2 Datacenter 简体中文 未激活',
-        'create_date'   :   1464135759,
-        'net_id'        :   ['net-004']
-    },{
-        'id'            :   'host-zasgt25a',
-        'name'          :   '开发1',
-        'status'        :   '运行中',
-        'net'           :   'www',
-        'ip'            :   '192.16.1.2',
-        'mirror'        :   'Windows Server 2008 R2 Datacenter 简体中文 未激活',
-        'create_date'   :   1464135759,
-        'net_id'        :   ['net-004']
+        'nets'          :   [{
+            'id' :'base-net',
+            'ip' : '192.168.0.1'
+        },{
+            'id' : 'net-001',
+            'ip' : '192.168.0.2'
+        },{
+            'id' : 'net-002',
+            'ip' : '192.168.0.2'
+        }]
     }
 ]
 
